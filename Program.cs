7d71dab870c9ffecac632f65799fa717d6b44f9d
Robbie <Robbie@DESKTOP-UAEN14H>
2021-06-25 09:07:16 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaxPay
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Taxpayer[] payers = new Taxpayer[10];
+            for (int i = 0; i < payers.Length; i++)
+            {
+                payers[i] = new Taxpayer();
+                Console.WriteLine("Enter payer #{0} SSN", i + 1);
+                payers[i].SocialSecNumber = Console.ReadLine();
+                Console.WriteLine("Enter payer #{0} yearly income", i + 1);
+                payers[i].YearlyGrossIncome = double.Parse(Console.ReadLine());
+            }
+            for (int i = 0; i < payers.Length; i++)
+            {
+                Console.WriteLine("Tax payer #{0}, SSN ={1}, Income = {2}, Tax Owed = {3}", i + 1, payers[i].SocialSecNumber, payers[i].YearlyGrossIncome, payers[i].TaxOwed);
+            }
+        }
+    }
+}
---
+++ TaxPayer.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaxPay
+{
+    class Taxpayer
+    {
+        private string socialSecNumber;
+        private double yearlyGrossIncome;
+        private double taxOwed;
+        public string SocialSecNumber
+        {
+            get
+            {
+                return socialSecNumber;
+            }
+            set
+            {
+                socialSecNumber = value;
+            }
+        }
+
+        public double YearlyGrossIncome
+        {
+            get
+            {
+                return yearlyGrossIncome;
+            }
+            set
+            {
+                yearlyGrossIncome = value;
+                calcTaxOwed();
+            }
+        }
+        public double TaxOwed
+        {
+            get
+            {
+                return taxOwed;
+            }
+        }
+        private void calcTaxOwed()
+        {
+            if (yearlyGrossIncome < 30000)
+            {
+                taxOwed = yearlyGrossIncome * .15;
+            }
+            else
+            {
+                taxOwed = yearlyGrossIncome * .28;
+            }
+        }
+    }
+}
